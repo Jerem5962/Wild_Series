8552b962ab1ba1f32e20f325090f773856dfccd7
Jerem5962 <jerem-62@outlook.com>
2019-11-20 17:33:14 +0100

    Ajout de la quete 5 de symfony

--- src/Controller/WildController.php
+++ src/Controller/WildController.php
@@ -17,4 +17,26 @@ class WildController extends AbstractController
             'website' => 'Wild Séries',
         ]);
     }
+
+    /**
+     * @Route("/wild/show/{slug<[a-z0-9-]+>?Aucune séries selectionées, veuillez choisir une série}", name="wild_show")
+     * @param $slug
+     * @return Response
+     */
+    public function show($slug): Response
+    {
+        $slug = ucwords(str_replace("-", " ", $slug)) ;
+
+        return $this->render('wild/show.html.twig', [
+            'text' => $slug
+        ]);
+    }
+
+    public function new(): Response
+    {
+        // traitement d'un formulaire par exemple
+
+        // redirection vers la page 'wild_show', correspondant à l'url wild/show/5
+        return $this->redirectToRoute('wild_show', ['page' => 5]);
+    }
 }
